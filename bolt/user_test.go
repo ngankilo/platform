3bc72c33e3eef75bb3fdbd6e7e3f59a49b92ec44
Michael Desa <mjdesa@gmail.com>
2018-11-29 12:46:21 -0800

    spike(platform): add kv backed user service

--- bolt/user_test.go
+++ bolt/user_test.go
@@ -5,25 +5,30 @@ import (
 	"testing"
 
 	"github.com/influxdata/platform"
+	"github.com/influxdata/platform/kv"
 	platformtesting "github.com/influxdata/platform/testing"
 )
 
 func initUserService(f platformtesting.UserFields, t *testing.T) (platform.UserService, func()) {
-	c, closeFn, err := NewTestClient()
+	s, closeFn, err := NewTestKVStore()
 	if err != nil {
-		t.Fatalf("failed to create new bolt client: %v", err)
+		t.Fatalf("failed to create new kv store: %v", err)
 	}
-	c.IDGenerator = f.IDGenerator
+	svc := kv.NewUserService(s, f.IDGenerator)
+	if err := svc.Initialize(); err != nil {
+		t.Fatalf("error initializing user service: %v", err)
+	}
+
 	ctx := context.Background()
 	for _, u := range f.Users {
-		if err := c.PutUser(ctx, u); err != nil {
+		if err := svc.PutUser(ctx, u); err != nil {
 			t.Fatalf("failed to populate users")
 		}
 	}
-	return c, func() {
+	return svc, func() {
 		defer closeFn()
 		for _, u := range f.Users {
-			if err := c.DeleteUser(ctx, u.ID); err != nil {
+			if err := svc.DeleteUser(ctx, u.ID); err != nil {
 				t.Logf("failed to remove users: %v", err)
 			}
 		}
